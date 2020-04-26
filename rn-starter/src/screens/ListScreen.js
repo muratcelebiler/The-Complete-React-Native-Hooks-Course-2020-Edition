86a95ad7b06d117c8faea3e58d8f4e2bc0e97823
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-04-27 00:58:19 +0300

    FlatList içerisine data gönderme ve önizleme

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+// FlatList dahil ediyoruz
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
-    return <Text>List screen page</Text>;
+    // Sabit bir array listesi oluşturuyoruz
+    const friends = [
+        { name: "Friend 1" },
+        { name: "Friend 2" },
+        { name: "Friend 3" },
+        { name: "Friend 4" },
+        { name: "Friend 5" }
+    ];
+
+    return (
+        /**
+         * FlatList'de iki field yardımı ile datayı listeleyebiliriz. Bunlar data ve renderItem
+         * data: Listelenecek array'i alır
+         * renderItem: data field'ine gelen datayı listelemek için kullanılır. Php'de yer akan array_map olayı gibi
+         */
+        <FlatList 
+            data={friends}
+            renderItem={({item}) => {
+                return <Text>{item.name}</Text>
+            }}
+        />
+    );
 };
 
 const styles = StyleSheet.create({
