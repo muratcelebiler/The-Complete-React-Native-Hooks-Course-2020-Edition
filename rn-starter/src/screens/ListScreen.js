09082743a18cab345e79074471ded8dd104ae9f3
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-04-27 01:08:03 +0300

    FlatList keyExtractor kullanımı

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -17,8 +17,10 @@ const ListScreen = () => {
          * FlatList'de iki field yardımı ile datayı listeleyebiliriz. Bunlar data ve renderItem
          * data: Listelenecek array'i alır
          * renderItem: data field'ine gelen datayı listelemek için kullanılır. Php'de yer akan array_map olayı gibi
+         * keyExtractor: Biz bu tanımlamayı yapmazsak friends array içerisinde herhangi bir data güncellenir veya silinerse react-native her defasında datayı tekrar build eder. keyExtractor ile her bir dataya unique bir key ataması yapıyoruz(Mysql'deki id kolonu gibi) Bu key sayesinde biz friends içerisinde herhangi bir datayı değişince react-native o item'e ait datayı key üzerinden tanıdığı için sadece o datayı günceller. Gelen datayı tekrar baştan buil etmez. Bu da performans açısından bizim için tercih edilmesi gereken yöntemdir!
          */
         <FlatList 
+            keyExtractor={friend => friend.name}
             data={friends}
             renderItem={({item}) => {
                 return <Text>{item.name}</Text>
