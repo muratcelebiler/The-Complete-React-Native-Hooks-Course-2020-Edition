801d86fac9a508cf385fbbc514053671b240f801
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-04-27 01:16:55 +0300

    Flatlist içerisinde datayı horizontal olarak listeleme ve scrollbar gizleme

--- rn-starter/src/screens/ListScreen.js
+++ rn-starter/src/screens/ListScreen.js
@@ -9,7 +9,11 @@ const ListScreen = () => {
         { name: "Friend 2" },
         { name: "Friend 3" },
         { name: "Friend 4" },
-        { name: "Friend 5" }
+        { name: "Friend 5" },
+        { name: "Friend 6" },
+        { name: "Friend 7" },
+        { name: "Friend 8" },
+        { name: "Friend 9" }
     ];
 
     return (
@@ -19,7 +23,9 @@ const ListScreen = () => {
          * renderItem: data field'ine gelen datayı listelemek için kullanılır. Php'de yer akan array_map olayı gibi
          * keyExtractor: Biz bu tanımlamayı yapmazsak friends array içerisinde herhangi bir data güncellenir veya silinerse react-native her defasında datayı tekrar build eder. keyExtractor ile her bir dataya unique bir key ataması yapıyoruz(Mysql'deki id kolonu gibi) Bu key sayesinde biz friends içerisinde herhangi bir datayı değişince react-native o item'e ait datayı key üzerinden tanıdığı için sadece o datayı günceller. Gelen datayı tekrar baştan buil etmez. Bu da performans açısından bizim için tercih edilmesi gereken yöntemdir!
          */
-        <FlatList 
+        <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
             keyExtractor={friend => friend.name}
             data={friends}
             renderItem={({item}) => {
