38dc41aabf2bd018098509b1e22dd711a6c8028d
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-04-26 16:54:28 +0300

    ListScreen component'inin oluşturulması

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -3,14 +3,16 @@ import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from "./src/screens/HomeScreen";
 // Oluşturduğumuz componenti import ediyoruz
 import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponentsScreen
+    Components: ComponentsScreen,
+    List: ListScreen
   },
   {
-    initialRouteName: "Components",
+    initialRouteName: "List",
     defaultNavigationOptions: {
       title: "App"
     }
