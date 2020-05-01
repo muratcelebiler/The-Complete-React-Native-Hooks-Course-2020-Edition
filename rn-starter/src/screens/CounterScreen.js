66c5bfbe8aad99891fdd6e46f538879e990cac50
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-02 02:10:58 +0300

    state kullanımı

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
+// state kullanabilmek için useState yapısını dahil ediyoruz
+import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 
 const CounterScreen = () => {
+  /**
+   * counter değişkenini değiştirmek için bir fonksiyon(setCounter) oluşturmamız gerekiyor.
+   * Oluşturduğumuz fonksiyonun adı set kelimesi ile başlamak zorunda değil! Herhangi bir isim yazabiliriz! Burada önemli olan ikinci parametrenin bir fonksiyon olduğunu anlamak.
+   * Set ettiğimiz fonksiyonda(setCounter) tanımladığımız değişkenin(counter) değerini değiştirebiliyoruz.
+   */
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Counter Default:</Text>
+      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
+      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
+      <Text>Counter Default: {counter}</Text>
     </View>
   );
 };
