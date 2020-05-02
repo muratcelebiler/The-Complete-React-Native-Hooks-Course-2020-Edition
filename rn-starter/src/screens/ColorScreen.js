cc2afe8ebe50d2554817f5ba2392897df11c9358
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-02 18:29:12 +0300

    Random color oluşturma

--- rn-starter/src/screens/ColorScreen.js
+++ rn-starter/src/screens/ColorScreen.js
@@ -4,9 +4,18 @@ import { View, Button, Text, StyleSheet } from "react-native";
 const ColorScreen = () => {
   return (
     <View>
-      <Text>Color screen page</Text>
+      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
     </View>
   );
+};
+
+// Rgb formatında random değerler üreten fonksiyon
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return `rgb(${red}, ${blue}, ${green})`;
 };
 
 const styles = StyleSheet.create({});
