819f83d13d1eadb6e14716984ea6b03109b8f911
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-04 23:53:46 +0300

    switch-case ve color box

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -2,29 +2,59 @@ import React, { useState } from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [blue, setBlue] = useState(0);
   const [green, setGreen] = useState(0);
 
+  const setColor = (color, change) => {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue + change < 0
+          ? null
+          : setBlue(blue + change);
+        return;
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      default:
+        return;
+    }
+  };
+
   return (
     <View>
       <ColorCounter
         color="Red"
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setColor("red", COLOR_INCREMENT)}
+        onDecrease={() => setColor("red", -COLOR_INCREMENT)}
       />
 
       <ColorCounter
         color="Blue"
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)}
+        onIncrease={() => setColor("blue", COLOR_INCREMENT)}
+        onDecrease={() => setColor("blue", -COLOR_INCREMENT)}
       />
 
       <ColorCounter
         color="Green"
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
+        onIncrease={() => setColor("green", COLOR_INCREMENT)}
+        onDecrease={() => setColor("green", -COLOR_INCREMENT)}
+      />
+
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${blue}, ${green})`,
+        }}
       />
     </View>
   );
