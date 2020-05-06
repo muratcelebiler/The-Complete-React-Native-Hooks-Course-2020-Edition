620c316869766ba19f4ff779c54c21b94bd5257f
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-07 00:11:03 +0300

    Reducer yapısınında değişken isim standartları (type,  payload)

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -5,13 +5,13 @@ import ColorCounter from "../components/ColorCounter";
 const COLOR_INCREMENT = 15;
 
 const reducer = (state, action) => {
-  switch (action.colorToChange) {
-    case "red":
-      return { ...state, red: state.red + action.amount };
-    case "green":
-      return { ...state, green: state.green + action.amount };
-    case "blue":
-      return { ...state, blue: state.blue + action.amount };
+  switch (action.type) {
+    case "change_red":
+      return { ...state, red: state.red + action.payload };
+    case "change_green":
+      return { ...state, green: state.green + action.payload };
+    case "change_blue":
+      return { ...state, blue: state.blue + action.payload };
     default:
       return state;
   }
@@ -25,30 +25,30 @@ const SquareScreen = () => {
       <ColorCounter
         color="Red"
         onIncrease={() =>
-          dispatch({ colorToChange: "red", amount: COLOR_INCREMENT })
+          dispatch({ type: "change_red", payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "red", amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: "change_red", payload: -1 * COLOR_INCREMENT })
         }
       />
 
       <ColorCounter
         color="Blue"
         onIncrease={() =>
-          dispatch({ colorToChange: "blue", amount: COLOR_INCREMENT })
+          dispatch({ type: "change_blue", payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "blue", amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: "change_blue", payload: -1 * COLOR_INCREMENT })
         }
       />
 
       <ColorCounter
         color="Green"
         onIncrease={() =>
-          dispatch({ colorToChange: "green", amount: COLOR_INCREMENT })
+          dispatch({ type: "change_green", payload: COLOR_INCREMENT })
         }
         onDecrease={() =>
-          dispatch({ colorToChange: "green", amount: -1 * COLOR_INCREMENT })
+          dispatch({ type: "change_green", payload: -1 * COLOR_INCREMENT })
         }
       />
 
