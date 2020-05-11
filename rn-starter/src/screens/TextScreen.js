5a8463ba2d9a60317e5a05e89bcfec3253b395fd
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-11 23:34:51 +0300

    TextScreen componentinin oluşturulması

---
+++ rn-starter/src/screens/TextScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const TextScreen = () => {
+  return <Text>Text Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TextScreen;
