b37aa627da72debd2267becaf47e2cf282bbe427
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-11 23:43:06 +0300

    TextInput eklenmesi ve  autoCapitalize, autoCorrect seçenekleri

--- rn-starter/src/screens/TextScreen.js
+++ rn-starter/src/screens/TextScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, TextInput } from "react-native";
 
 const TextScreen = () => {
-  return <Text>Text Screen</Text>;
+  return (
+    /**
+     * autoCapitalize: Bu property ile iOS'daki otomatik ilk harfi büyütme seçeneğini kapatıyoruz
+     * autoCorrect: Bu property ile iOS'daki otomatik tamamlama özelliğini kapatıyoruz
+     */
+    <TextInput style={styles.input} autoCapitalize="none" autoCorrect={false} />
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    borderColor: "black",
+    margin: 15,
+    borderWidth: 1,
+  },
+});
 
 export default TextScreen;
