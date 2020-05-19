21c44e5f07f60e287142e33a64b2e633ed536b2a
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-19 11:38:19 +0300

    SearchScreen componentinin oluşturulması ve Stack navigator

---
+++ restaurant-search-app/src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search screen test</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
