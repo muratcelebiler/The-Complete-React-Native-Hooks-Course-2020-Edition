f1b2605a82cb1451e5a8193c8e40e05638f7a0f5
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-21 22:22:29 +0300

    TextInput eklenmesi ve css yapılandırması

--- restaurant-search-app/src/components/SearchBar.js
+++ restaurant-search-app/src/components/SearchBar.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 
 const SearchBar = () => {
     return (
-        <View style={styles.background}>
-            <Feather name="search" size={30} />
-            <Text>Search bar screen</Text>
+        <View style={styles.backgroundStyle}>
+            <Feather name="search" size={30} style={styles.iconStyle} />
+            <TextInput placeholder="Search" style={styles.inputStyle}/>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
+        marginTop: 10,
         backgroundColor: "#CCC",
         height: 50,
         borderRadius:5,
-        marginHorizontal:15
+        marginHorizontal:15,
+        flexDirection:'row',
+    },
+    inputStyle: {
+        flex:1,
+        fontSize: 18
+    },
+    iconStyle: {
+        alignSelf: 'center',
+        marginHorizontal: 10
     }
 });
 
