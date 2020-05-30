732c81cdbff3f0dcc5ffd27afe039beafbbebed6
Murat Çelebiler <muratcelebiler@teknasyon.com>
2020-05-30 12:54:37 +0300

    Error handler yapısı, try-catch bloğu

--- restaurant-search-app/src/screens/SearchScreen.js
+++ restaurant-search-app/src/screens/SearchScreen.js
@@ -6,16 +6,22 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const SearchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: "san jose"
-            }
-        });
-        setResults(response.data.businesses)
+        console.log("Hi There")
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: "san jose"
+                }
+            });
+            setResults(response.data.businesses)
+        } catch(err) {
+            setErrorMessage("Something went wrong!")
+        }
     };
 
     return (
@@ -26,7 +32,7 @@ const SearchScreen = () => {
                 onTermSubmit={SearchApi}
              />
             <Text>We have found {results.length} results</Text>
-            <Text>{term}</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
         </View>
     );
 };
